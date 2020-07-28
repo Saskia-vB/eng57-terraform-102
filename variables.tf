7923c0c76219cdc94e242fbd1317133ab671194b
Saskia van Barthold <saskia.vanb@gmail.com>
2020-07-28 15:09:51 +0200

    personalised teh code with variables

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "vpc_id" {
+  type = string
+  default= "vpc-08039043ffb902e94"
+}
+
+variable "name" {
+  type = string
+  default= "Eng57.Saskia.vB."
+}
+
+variable "ami-web" {
+  type = string
+  default= "ami-00b48f09c568b0014"
+}
+
+variable "ami-db" {
+  type = string
+  default= ""
+}
